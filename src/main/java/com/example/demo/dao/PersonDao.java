44c667980f8ce868923b4da663a5a9b1850dca52
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-01-12 13:23:10 -0500

    This allows us to generate a random UUID

--- src/main/java/com/example/demo/dao/PersonDao.java
+++ src/main/java/com/example/demo/dao/PersonDao.java
@@ -5,4 +5,9 @@ import java.util.UUID:
 public interface PersonDao {
 
     int insertPerson(UUID id, Person person);
+
+    default int addPerson(Person person){
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
 }
