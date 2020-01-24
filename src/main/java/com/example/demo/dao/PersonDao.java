b2b4b3e7cc68dd3067da395e80cbe73091267e42
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-01-24 12:52:22 -0500

    Preparing structure for response to Post request

--- src/main/java/com/example/demo/dao/PersonDao.java
+++ src/main/java/com/example/demo/dao/PersonDao.java
@@ -1,5 +1,7 @@
 package com.example.demo.dao;
 import com.example.demo.model.Person;
+
+import java.util.List;
 import java.util.UUID;
 
 public interface PersonDao {
@@ -10,4 +12,5 @@ public interface PersonDao {
         UUID id = UUID.randomUUID();
         return insertPerson(id, person);
     }
+    List<Person> selectAllPeople();
 }
