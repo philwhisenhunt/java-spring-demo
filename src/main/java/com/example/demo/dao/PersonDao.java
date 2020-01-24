9d5337b8db4d869016ae7387b495087f071d65f8
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-01-24 13:09:43 -0500

    Now implementing methods by using alt enter

--- src/main/java/com/example/demo/dao/PersonDao.java
+++ src/main/java/com/example/demo/dao/PersonDao.java
@@ -2,6 +2,7 @@ package com.example.demo.dao;
 import com.example.demo.model.Person;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PersonDao {
@@ -13,6 +14,8 @@ public interface PersonDao {
         return insertPerson(id, person);
     }
     List<Person> selectAllPeople();
+    Optional<Person> selectPersonById(UUID id);
 
     int deletePersonById(UUID id);
+    int updatePersonById(UUID id, Person person);
 }
