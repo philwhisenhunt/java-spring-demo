4cea635ff2fd3a83c5a4bc1af807d3db7d54b9d6
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-01-24 12:42:11 -0500

    Added enough to run the program and then get a response back in Postman

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -4,7 +4,11 @@ import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/v1/person")
 
 @RestController
 public class PersonController {
@@ -15,8 +19,9 @@ public class PersonController {
         this.personService = personService;
     }
 
+    //by adding the @RequestBody thing, we are saying that we want to turn the received Json object into a person
     @PostMapping
-    public void addPerson(Person person) {
+    public void addPerson(@RequestBody Person person) {
         personService.addPerson(person);
     }
 }
