79aceb97082c8ec98da6fc9b2d6bc3ad627d9bfc
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-01-30 12:00:02 -0500

    Building out PostgresDatasource class

--- src/main/java/com/example/demo/datasource/PostgresDataSource.java
+++ src/main/java/com/example/demo/datasource/PostgresDataSource.java
@@ -1,4 +1,21 @@
 package com.example.demo.datasource;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.jdbc.metadata.HikariDataSourcePoolMetadata;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
 public class PostgresDataSource {
+
+    @Bean
+    @ConfigurationProperties("app.datasource")
+    public HikariDataSourcePoolMetadata hikariDataSource {
+        return DataSourceBuilder
+                .create()
+                .type(HikariDataSourcePoolMetadata.class)
+                .build();
+    }
 }
